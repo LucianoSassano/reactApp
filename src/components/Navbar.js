404f0d18acbfffccf12b007bfcd01bd22a809655
Luciano_Sassano <lucianosassano1@gmail.com>
2020-02-08 01:39:09 -0300

    applying materialize framework

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,35 @@
 import React, { Component } from "react";
+import fondo from './imagenes/fondo.jpg';
+import profile from './imagenes/profile.jpeg';
+
+
 
 class Navbar extends Component {
+  componentDidMount() {
+    const M = window.M;
+
+    document.addEventListener("DOMContentLoaded", function() {
+      var elems = document.querySelectorAll(".sidenav");
+      var instances = M.Sidenav.init(elems, {});
+    });
+  }
+
   render() {
     return (
       <div>
         <nav>
-          <div class="nav-wrapper">
-            <a href="#" class="brand-logo right">
+          <div className="nav-wrapper">
+            <a
+              href="#"
+              data-target="slide-out"
+              className="sidenav-trigger show-on-large"
+            >
+              <i className="material-icons">menu</i>
+            </a>
+            <a href="#" className="brand-logo right">
               Logo
             </a>
-            <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <ul id="nav-mobile" className="left hide-on-med-and-down">
               <li>
                 <a href="sass.html">Sass</a>
               </li>
@@ -22,6 +42,44 @@ class Navbar extends Component {
             </ul>
           </div>
         </nav>
+
+        <ul id="slide-out" className="sidenav">
+          <li>
+            <div className="user-view">
+              <div className="background">
+                <img src={fondo}></img>
+              </div>
+              <a href="#user">
+                <img className="circle" src={profile} alt="profile pic" />
+              </a>
+              <a href="#name">
+                <span className="white-text name">John Doe</span>
+              </a>
+              <a href="#email">
+                <span className="white-text email">jdandturk@gmail.com</span>
+              </a>
+            </div>
+          </li>
+          <li>
+            <a href="#!">
+              <i className="material-icons">cloud</i>First Link With Icon
+            </a>
+          </li>
+          <li>
+            <a href="#!">Second Link</a>
+          </li>
+          <li>
+            <div className="divider"></div>
+          </li>
+          <li>
+            <a className="subheader">Subheader</a>
+          </li>
+          <li>
+            <a className="waves-effect" href="#!">
+              Third Link With Waves
+            </a>
+          </li>
+        </ul>
       </div>
     );
   }
